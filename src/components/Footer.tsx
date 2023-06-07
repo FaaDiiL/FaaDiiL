d006522bd868d902b0e7fe8e00b572a2f5a84ee8
FaaDiiL <fadil.alfarhan@gmail.com>
2023-06-07 22:02:35 +0200

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer>
+      <small>By Fadil Al Farhan</small>
+    </footer>
+  )
+}
+
+export default Footer
